44bdd89e6e23438eaa96f76c620dbba62a091a38
sheblair <66489366+sheblair@users.noreply.github.com>
2023-05-23 20:17:54 -0400

    homepage desktop styles done

--- front-page.php
+++ front-page.php
@@ -10,24 +10,34 @@
 
 get_header();
 ?>
+	<div class="page-container">
+		<?php get_sidebar(); ?>
 
-	<main id="primary" class="site-main">
+		<main id="primary" class="site-main">
+			<?php
+			while ( have_posts() ) : the_post();
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+				// get_template_part( 'template-parts/content', 'page' );
 
-			get_template_part( 'template-parts/content', 'page' );
+				// // If comments are open or we have at least one comment, load up the comment template.
+				// if ( comments_open() || get_comments_number() ) :
+				// 	comments_template();
+				// endif;
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+				$homepage_text = get_field('homepage_text');
+				$homepage_image = get_field('homepage_image');
+				$size = 'full';
 
-		endwhile; // End of the loop.
-		?>
+			?>
 
+			<div id="homepage">
+				<p class="homepage-text"><?php echo $homepage_text; ?></p>
+				<figure class="homepage-img">
+					<?php echo wp_get_attachment_image($homepage_image, $size) ?>
+				</figure>
+			</div>
+
+			<?php endwhile; ?> <!-- End of the loop -->
 	</main><!-- #main -->
-
-<?php
-get_sidebar();
+	</div>
+	
