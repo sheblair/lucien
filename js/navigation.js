fa6b3fae3b07df67ac4e7af7a44480526b98f06e
sheblair <66489366+sheblair@users.noreply.github.com>
2023-05-26 16:23:25 -0400

    mobile off-canvas menu core functionality

--- js/navigation.js
+++ js/navigation.js
@@ -47,9 +47,9 @@ document.addEventListener('DOMContentLoaded', function() {
 	
 		// Remove the .toggled class and set aria-expanded to false when the user clicks outside the navigation.
 		document.addEventListener( 'click', function( event ) {
-			const isClickInside = siteNavigation.contains( event.target );
+			const isClickInside = menu.contains( event.target );
 	
-			if ( ! isClickInside ) {
+			if ( isClickInside ) {
 				siteNavigation.classList.remove( 'toggled' );
 				button.setAttribute( 'aria-expanded', 'false' );
 			}
