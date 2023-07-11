df0980339984de415c1e918d15a5d94212928fb2
sheblair <66489366+sheblair@users.noreply.github.com>
2023-07-11 13:55:50 -0400

    add single-offering.php

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,26 +10,6 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				lucien_posted_on();
-				lucien_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
-
-	<?php lucien_post_thumbnail(); ?>
 
 	<div class="entry-content">
 		<?php
@@ -57,7 +37,4 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php lucien_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
